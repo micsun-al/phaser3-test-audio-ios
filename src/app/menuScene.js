1940e22e7999bbbd7f0d6a679c16e9f82f724c37
micsun-al <michael@avatarlabs.com>
2019-10-08 15:16:05 -0700

    Added some clarification to what's happening.

--- src/app/menuScene.js
+++ src/app/menuScene.js
@@ -1,19 +1,18 @@
-export default {
+export default class extends Phaser.Scene {
+  constructor() { super({ key: 'menu' }) }
 
-  key: 'menu',
-
-  create: function () {
+  create() {
     this.add.image(400, 150, 'star').setScale(5);
-    this.add.text(400, 300, 'Phaser 3 with Parcel\n\n(play)', {
+
+    this.add.text(400, 300, 'Phaser 3 iOS AUDIO TEST\n\n(play)', {
       align: 'center',
       fill: 'lime',
       fontFamily: 'sans-serif',
       fontSize: 48
     })
       .setOrigin(0.5, 0);
-    this.input.on('pointerdown', function () {
+    this.input.on('pointerup', function (a, b, c, d) {
       this.scene.switch('play');
-    }, this);
+    }, this)
   }
-
-};
+}
