067e85189f6eb25301ee7c42c50da1dbd1bd4e4d
micsun-al <michael@avatarlabs.com>
2019-10-11 14:24:13 -0700

    Updated to Phaser 3.20 (still reproduces).

--- src/app/playScene.js
+++ src/app/playScene.js
@@ -6,12 +6,13 @@ export default class extends Phaser.Scene {
   create() {
     this.star = this.add.image(this.cameras.main.centerX, this.cameras.main.height * 0.4, 'star')
       .setScale(10)
+      .setInteractive({ cursor: "pointer" })
     this.text = this.add.text(this.cameras.main.centerX, this.cameras.main.height * 0.415, "LINK CLICK")
       .setOrigin(0.5)
       .setTint(0x000000)
     this.text = this.add.text(this.cameras.main.centerX, this.cameras.main.height * 0.7, "On iOS, the audio stops playing after returning from the link/new tab.")
       .setOrigin(0.5)
       .setTint(0x00ffff)
-    this.input.on('pointerup', () => { window.open('https://lmgtfy.com/?q=cats', '_blank') })
+    this.star.on('pointerup', () => { window.open('https://lmgtfy.com/?q=cats', '_blank') })
   }
 };
